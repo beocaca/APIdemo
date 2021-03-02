47914265c8c173cf15b793e6f16f0d86edcb39a2
Nam <ceonamnguyen@gmail.com>
2021-03-02 11:06:31 +0700

    awww

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import 'regenerator-runtime/runtime'
+/*
 
 var courseAPI = 'https://603713ec54350400177218c0.mockapi.io/users'
 var usersAPI = []
@@ -20,7 +21,7 @@ function renderCourse(courses) {
     <button onclick="handleDeleteCourse(${course.id})">DELETE</button>
     <button onclick="handleEditCourse(${course.id})">EDIT</button>
     </li>
-  `
+    `
     })
     listCourseBlock.innerHTML = htmls.join('')
   }
@@ -174,9 +175,11 @@ window.onload = () => {
   window.showModal = function showModal() {
     modal.style.display = 'block'
   }
+
   window.closeModal = function closeModal() {
     modal.style.display = "none";
   }
+
   window.onclick = function (event) {
     if (event.target === modal) {
       modal.style.display = "none";
@@ -195,9 +198,181 @@ window.onload = () => {
     handleCreateForm()
   }
 
-  start()
+  /!*
+    start()
+  *!/
 
-  setTimeout(() => {
-    console.log(usersAPI)
-  }, 2000)
 }
+*/
+
+
+var API = 'https://603713ec54350400177218c0.mockapi.io/users'
+const axios = require('axios').default;
+var list = []
+
+function getUsers() {
+  axios.get(API)
+    .then(function (response) {
+      // handle success
+      list = [...response.data]
+      renderUsers(list)
+      console.log(list)
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+}
+
+function renderUsers(listUsers) {
+  var listBlock = document.querySelector('#list-course')
+  var htmls = listUsers.map(e => {
+    return `
+    <li>
+  <h2>id : ${e.id} </h2>
+  <h3>Name :  ${e.name} </h3>
+  <h4> Job :  ${e.job} </h4>
+  <button onclick="btnDelete(${e.id})"> Xoá</button>
+  <button onclick="btnEdit(${e.id})">Sửa</button>
+    </li>
+    `
+  })
+  listBlock.innerHTML = htmls.join('')
+}
+
+window.btnDelete = function btnDelete(id) {
+  if (window.confirm('Xoá hả ?')) {
+
+    document.querySelector(`button[onclick = "btnDelete(${id})"]`).style.display = 'none'
+    document.querySelector(`button[onclick = "btnEdit(${id})"]`).style.display = 'none'
+
+    axios.delete(API + '/' + id)
+      .then(function (response) {
+
+        var indexId = list.findIndex(e => {
+          return e.id === `${id}`
+        })
+
+        list.splice(indexId, 1)
+        renderUsers(list)
+
+        console.log(list)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }
+}
+
+window.btnEdit = function btnEdit(id) {
+  var modal = (document.querySelector('#myModal'))
+
+  modal.style.display = 'block'
+
+  var a = list.find(e => {
+    return e.id === `${id}`
+  })
+
+  var nameInput = document.querySelector('input[name = "nameEdit"]')
+  var jobInput = document.querySelector('input[name = "jobEdit"]')
+  nameInput.value = a.name
+  jobInput.value = a.job
+  document.querySelector('#save').onclick = (e) => {
+    if (validate(nameInput.value)) {
+      axios.put(API + '/' + id, {
+        name: nameInput.value,
+        job: jobInput.value
+      })
+        .then(function (response) {
+          var index = list.findIndex(e => {
+            return e.id === `${id}`
+          })
+          list[index] = response.data
+          renderUsers(list)
+          console.log(response.data)
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+        .then(closeModal)
+    }
+    closeModal()
+  }
+
+  function closeModal() {
+    modal.style.display = 'none'
+  }
+
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+
+function validate(name) {
+  if (!name || !name.trim() || name.trim().length < 3) {
+    alert("Invalid Name")
+    return false;
+  }
+  return name;
+}
+
+function addUser() {
+  var btnCreate = document.querySelector('#create')
+  btnCreate.onclick = (e => {
+    var newName = document.querySelector('input[name = "name"]').value
+    var newJob = document.querySelector('input[name = "job"]').value
+    if (validate(newName)) {
+      btnCreate.style.display = 'none'
+      axios.post(API, {
+        name: newName,
+        job: newJob
+      })
+        .then(function (response) {
+          var newUser = response.data
+          list.push(newUser)
+          renderUsers(list)
+          console.log(list)
+        })
+        .catch(function (error) {
+          console.log(error);
+        }).then(() => {
+        btnCreate.style.display = 'block'
+      })
+    }
+  })
+}
+
+window.onload = () => {
+
+  getUsers()
+
+  addUser()
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
